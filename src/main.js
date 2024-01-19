bd68c7582c123e7d7a3264d5189dfc4d9cd4f64b
Rosalind Dixon <romdixon@ucsc.edu>
2024-01-19 10:09:14 -0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+// Code Practice: Making a Scene
+// Name: Rosalind Dixon
+// Date: 01-18-24
+
+"use strict"
+
+let config = {
+    type: Phaser.AUTO,
+    scene: [ MainMenu, Play ]
+}
+
+let game = new Phaser.Game(config)
